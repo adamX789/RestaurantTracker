e2093e7eaa716908ef600f272939be3cc05c1ea1
Adam Sorf <adamsorf99@gmail.com>
2025-08-06 17:15:09 +0200

    Úprava pro Railway

--- main/Procfile
+++ main/Procfile
@@ -1,1 +1,1 @@
-web: gunicorn main.wsgi --bind 0.0.0.0:$PORT
+web: gunicorn main.wsgi
--- main/restaurants/forms.py
+++ main/restaurants/forms.py
@@ -1,16 +1,15 @@
 from django import forms
 from .models import *
 
+type_list = []
+for res in Restaurant.objects.all():
+    type_ = res.type
+    if type_ not in type_list:
+        type_list.append(type_)
+choices = [("Všechny", "Všechny")]
+for type_ in type_list:
+    choices.append((type_, type_))
+
 
 class MapForm(forms.Form):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # Databázový dotaz probíhá až zde, když se formulář vytváří
-        type_list = set(Restaurant.objects.values_list('type', flat=True))
-
-        choices = [("Všechny", "Všechny")]
-        for type_ in type_list:
-            choices.append((type_, type_))
-
-        self.fields['type'] = forms.ChoiceField(choices=choices, required=True)
+    type = forms.ChoiceField(choices=choices,required=True)
